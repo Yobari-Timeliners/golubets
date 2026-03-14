94811656121eb15a9e5f058db55b453f6507d604
Fedor Blagodyr <bagotir@gmail.com>
2026-03-14 18:31:17 +0300

    format

--- script/tool/test/repo_package_info_check_command_test.dart
+++ script/tool/test/repo_package_info_check_command_test.dart
@@ -134,12 +134,13 @@ ${readmeTableEntry(pluginName)}
 
       // 4 packages * 2 checks (git, gh) = 8 calls.
       // Default mocks in setUp cover 1 call each. We need 3 more each.
-      gitProcessRunner.mockProcessesForExecutable['git-ls-remote']!
-          .addAll(<FakeProcessInfo>[
-            FakeProcessInfo(MockProcess(exitCode: 1)),
-            FakeProcessInfo(MockProcess(exitCode: 1)),
-            FakeProcessInfo(MockProcess(exitCode: 1)),
-          ]);
+      gitProcessRunner.mockProcessesForExecutable['git-ls-remote']!.addAll(
+        <FakeProcessInfo>[
+          FakeProcessInfo(MockProcess(exitCode: 1)),
+          FakeProcessInfo(MockProcess(exitCode: 1)),
+          FakeProcessInfo(MockProcess(exitCode: 1)),
+        ],
+      );
 
       final List<String> output = await runCapturingPrint(runner, <String>[
         'repo-package-info-check',
