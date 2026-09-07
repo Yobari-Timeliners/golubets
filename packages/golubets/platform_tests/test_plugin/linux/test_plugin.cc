da1610f01fc3b6eec958af76e1e7c53f3f667c75
Fedor Blagodyr <bagotir@gmail.com>
2026-09-07 16:53:45 +0300

    fix linux unit-tests

--- packages/golubets/platform_tests/test_plugin/linux/test_plugin.cc
+++ packages/golubets/platform_tests/test_plugin/linux/test_plugin.cc
@@ -326,7 +326,8 @@ create_nested_nullable_string(const gchar* nullable_string,
   FlValue* classMap = fl_value_new_map();
   g_autoptr(CoreTestsGolubetsTestAllClassesWrapper) wrapper =
       core_tests_golubets_test_all_classes_wrapper_new(
-          types, nullptr, nullptr, classList, nullptr, classMap, nullptr);
+          types, nullptr, nullptr, classList, nullptr, classMap, nullptr,
+          nullptr);
   return core_tests_golubets_test_host_integration_core_api_create_nested_nullable_string_response_new(
       wrapper);
 }
